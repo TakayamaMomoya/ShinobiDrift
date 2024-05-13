4df8117552904137c0ce2551a699ee39b517cd0d
MorikawaShunya <221014@st.yoshida-g.ac.jp>
2024-05-13 14:55:22 +0900

    プレイヤー停止時の慣性調整

--- project/code/player.cpp
+++ project/code/player.cpp
@@ -24,7 +24,7 @@
 namespace
 {
 const std::string PATH_PARAM = "data\\TEXT\\playerParam.txt";	// パラメーターデータのパス
-const float fNotrot = 1.0f;	// 回転しないようにする速度
+const float NOTROTATE = 1.0f;		// 回転しないようにする値
 }
 
 //*****************************************************
@@ -277,7 +277,7 @@ void CPlayer::InputCamera(void)
 //=====================================================
 void CPlayer::ManageSpeed(void)
 {
-	CSlow *pSlow = CSlow::GetInstance();
+	CSlow* pSlow = CSlow::GetInstance();
 
 	// スピードの増減
 	if (m_info.fSpeedDest >= m_info.fSpeed)
@@ -311,7 +311,7 @@ void CPlayer::ManageSpeed(void)
 		SetPosition(pos);
 	}
 
-	if (m_info.fSpeed >= fNotrot)
+	if (m_info.fSpeed >= NOTROTATE)
 	{// ハンドルの回転を追加
 		rot.y += m_info.fAngleHandle * m_param.fAngleMaxCurve;
 	}
