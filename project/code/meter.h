c34cfc6fc2efbbec40ff592dc5b375aa945625e3
MorikawaShunya <221014@st.yoshida-g.ac.jp>
2024-05-21 10:17:20 +0900

    メーター表示。timerのコメント、いらん関数削除

--- project/code/meter.h
+++ project/code/meter.h
@@ -1,18 +1,19 @@
 //*****************************************************
 //
-// タイムの処理[timer.h]
+// メーター[meter.h]
 // Author:森川駿弥
 //
 //*****************************************************
 
-#ifndef _TIMER_H_
-#define _TIMER_H_
+#ifndef _METER_H_
+#define _METER_H_
 
 //*****************************************************
 // インクルード
 //*****************************************************
 #include "main.h"
 #include "number.h"
+#include "player.h"
 
 //*****************************************************
 // マクロ定義
@@ -22,24 +23,26 @@
 //*****************************************************
 // クラスの定義
 //*****************************************************
-class CTimer : public CObject
+class CMeter : public CObject
 {
 public:
-	CTimer(int nPriority = 7);	// コンストラクタ
-	~CTimer();	// デストラクタ
+	CMeter(int nPriority = 7);	// コンストラクタ
+	~CMeter();	//	デストラクタ
 
 	// メンバ関数
-	static CTimer* Create();	// 生成
+	static CMeter* Create();	// 生成
 
 	HRESULT Init();
 	void Uninit();
 	void Update();
-	void Sub(int nTime) { m_nSeconds -= nTime; }
+	void Acceleration();
+	void Deceleration();
 
 private:
 	//メンバ変数
-	int m_nSeconds;		// 現在の時間
-	int m_nCntSeconds;	// カウント加算
+	float m_nMeter;		// 現在のメーター値
+	int m_nCntMeter;	// カウント加算
 	CNumber* m_pNumber;	// ナンバーのポインタ
+	CPlayer* m_pPlayer;	// プレイヤーのポインタ
 };
 #endif
