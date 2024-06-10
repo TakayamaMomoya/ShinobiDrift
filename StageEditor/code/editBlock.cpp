02caccd9a50e1a900b8c1c2276113632c737a159
TakayamaMomoya <119379245+TakayamaMomoya@users.noreply.github.com>
2024-06-10 09:25:41 +0900

    ステージエディターで置いたブロックが表示されないバグの修正と、回転エリアの調整時の値制限実装

--- StageEditor/code/editBlock.cpp
+++ StageEditor/code/editBlock.cpp
@@ -311,6 +311,7 @@ void CStateCreateBlockNormal::CreateBlock(D3DXVECTOR3 pos)
 
 	if (pBlock != nullptr)
 	{
+		pBlock->BindModel(pInfoBlock[m_nIdxObject].nIdxModel);
 		pBlock->SetPosition(pos);
 		pBlock->SetRot(m_pObjectCursor->GetRotation());
 		pBlock->SetIdx(m_nIdxObject);
@@ -437,8 +438,8 @@ void CStateEditGrabBlock::EditGrabBlock(void)
 	float fAngle1 = (*m_it)->GetAngleOffset(0);
 	float fAngle2 = (*m_it)->GetAngleOffset(1);
 
-	ImGui::DragFloat("Angle1", &fAngle1, 0.05f, -D3DX_PI, D3DX_PI);
-	ImGui::DragFloat("Angle2", &fAngle2, 0.05f, -D3DX_PI, D3DX_PI);
+	ImGui::DragFloat("Angle1", &fAngle1, 0.05f, fAngle2, D3DX_PI);
+	ImGui::DragFloat("Angle2", &fAngle2, 0.05f, -D3DX_PI, fAngle1);
 
 	(*m_it)->SetAngleOffset(fAngle1, 0);
 	(*m_it)->SetAngleOffset(fAngle2, 1);
