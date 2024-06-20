2c4e8c6acc13f3d1cdb9131a3ca47d3559dd72a7
TakayamaMomoya <119379245+TakayamaMomoya@users.noreply.github.com>
2024-06-20 10:48:22 +0900

    エディターのバグ修正

---
+++ StageEditor/code/tunnel.h
@@ -0,0 +1,47 @@
+//*****************************************************
+//
+// トンネルの処理[tunnel.h]
+// Author:髙山桃也
+//
+//*****************************************************
+#ifndef _TUNNEL_H_	// 二重インクルード防止
+#define _TUNNEL_H_
+
+//*****************************************************
+// インクルード
+//*****************************************************
+#include "meshCylinder.h"
+#include "meshRoad.h"
+
+//*****************************************************
+// クラス定義
+//*****************************************************
+class CTunnel : public CMeshCylinder
+{
+public:
+	CTunnel(int nPriority = 5);	// コンストラクタ
+	~CTunnel();	// デストラクタ
+
+	// メンバ変数
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	// 変数取得・設定関数
+	std::vector<CMeshRoad::SInfoRoadPoint>::iterator GetItStart(void) { return m_itStart; }
+	std::vector<CMeshRoad::SInfoRoadPoint>::iterator GetItEnd(void) { return m_itEnd; }
+
+	// 静的メンバ関数
+	static CTunnel *Create(std::vector<CMeshRoad::SInfoRoadPoint>::iterator itStart, std::vector<CMeshRoad::SInfoRoadPoint>::iterator itEnd);
+
+private:
+	// メンバ関数
+	void CreateVtx(void);	// 頂点の生成
+	void VtxFollowRoad(void);	// 頂点を道に沿わせる
+
+	// メンバ変数
+	std::vector<CMeshRoad::SInfoRoadPoint>::iterator m_itStart;	// 開始イテレイター
+	std::vector<CMeshRoad::SInfoRoadPoint>::iterator m_itEnd;	// 終了イテレイター
+};
+#endif
