9717509f6a72683f2e7a7ee664026dd6cac76726
ryosukeohara <221109@st.yoshida-g.ac.jp>
2024-07-02 12:04:52 +0900

    敵をバイクに乗せた。

--- project/code/enemy.h
+++ project/code/enemy.h
@@ -25,12 +25,6 @@ class CEnemy : public CMotion
 {
 public:
 
-	struct SInfoRoadPoint
-	{// 辺に必要な情報
-		D3DXVECTOR3 pos;
-		SInfoRoadPoint() : pos({ 0.0f,0.0f,0.0f }) {}
-	};
-
 	CEnemy(int nPriority = 4);	// コンストラクタ
 	~CEnemy();	                // デストラクタ
 
@@ -38,28 +32,28 @@ public:
 	void Uninit(void);          // 終了処理
 	void Update(void);          // 更新処理
 	void Draw(void);            // 描画処理
-	static CEnemy* Create();      // 生成処理
+	static CEnemy* Create();    // 生成処理
 
 private:
 
 	// 状態
 	enum STATE
 	{
-		STATE_NONE = 0,	// 何でもない状態
-		STATE_NORMAL,	// 通常状態
+		STATE_NONE = 0,	         // 何でもない状態
+		STATE_NORMAL,	         // 通常状態
 		STATE_MAX
 	};
 
 	// 情報
 	struct SInfo
 	{
-		STATE state;   // 状態
-		float fSpeed;  // 速さ
+		STATE state;             // 状態
+		float fSpeed;            // 速さ
 	};
 
 	SInfo m_Info;                     // 情報
 	static CEnemy *m_pEnemy;          // 自分自身のポインタ
-	CMotion* m_pEnemyNinja;          // バイクに乗っている忍者
+	CMotion* m_pEnemyNinja;           // バイクに乗っている忍者
 	CCutMullSpline *m_pSpline;	      // スプライン
 	std::vector<D3DXVECTOR3> m_vPos;  // データ点のベクター
 
