8649401667465a568dca54e5d329acd40bb7e8c5
MorikawaShunya <221014@st.yoshida-g.ac.jp>
2024-07-04 10:10:19 +0900

    ミリ秒の表示できたよ

--- project/code/timer.cpp
+++ project/code/timer.cpp
@@ -17,9 +17,13 @@
 //*****************************************************
 namespace
 {
-const int TIME_PLACE = 3;		// 桁数
-const int TIME_LIMIT = 5;		// タイムの上限値
-const float TIME_WIDTH = SCREEN_WIDTH * 0.5f;		// 幅
+const int SECOND_PLACE = 3;			// 秒の桁数
+const int SECOND_LIMIT = 300;		// 秒の上限値
+const float SECOND_WIDTH = SCREEN_WIDTH * 0.5f;		// 秒のX座標
+const int MILLI_PLACE = 2;			// ミリ秒の桁数
+const int MILLI_LIMIT = 60;			// ミリ秒の上限
+const float MILLI_WIDTH = SCREEN_WIDTH * 0.65f;
+const int TIME_CALCULATION = 60;	// 秒計算用
 }
 
 //*****************************************************
@@ -32,8 +36,10 @@ CTimer* CTimer::m_pTimer = nullptr;
 //=====================================================
 CTimer::CTimer(int nPriority) : CObject(nPriority)
 {
-	m_fSeconds = 0.0f;		// 現在の時間
-	m_pNumber = nullptr;	// ナンバーのポインタ
+	m_fSecond = 0.0f;			// 現在の時間(秒)
+	m_fMsecond = 0.0f;			// 現在の時間(ミリ秒)
+	m_pSecond = nullptr;		// 秒表示用のポインタ
+	m_pMilliSecond = nullptr;	// ミリセカンド表示用のポインタ
 }
 
 //=====================================================
@@ -48,13 +54,41 @@ CTimer::~CTimer()
 //=====================================================
 CTimer* CTimer::Create(void)
 {
-	// インスタンス生成
-	CTimer* pTimer = new CTimer;
+	if (m_pTimer == nullptr)
+	{// インスタンス生成
+		m_pTimer = new CTimer;
+	}
 
-	// 初期化
-	pTimer->Init();
+	if (m_pTimer != nullptr)
+	{// 初期化
+		m_pTimer->Init();
 
-	return pTimer;
+		if (m_pTimer->m_pSecond == nullptr)
+		{// 秒表示
+			// 生成
+			m_pTimer->m_pSecond = CNumber::Create(SECOND_PLACE, SECOND_LIMIT);
+
+			// 位置
+			m_pTimer->m_pSecond->SetPosition(D3DXVECTOR3(SECOND_WIDTH, 75.0f, 0.0f));
+
+			// サイズ
+			m_pTimer->m_pSecond->SetSizeAll(35.0f, 35.0f);
+		}
+
+		if (m_pTimer->m_pMilliSecond == nullptr)
+		{// ミリ秒表示
+			// 生成
+			m_pTimer->m_pMilliSecond = CNumber::Create(MILLI_PLACE, MILLI_LIMIT);
+
+			// 位置
+			m_pTimer->m_pMilliSecond->SetPosition(D3DXVECTOR3(MILLI_WIDTH, 85.0f, 0.0f));
+
+			// サイズ
+			m_pTimer->m_pMilliSecond->SetSizeAll(25.0f,15.0f);
+		}
+	}
+
+	return m_pTimer;
 }
 
 //=====================================================
@@ -62,20 +96,10 @@ CTimer* CTimer::Create(void)
 //=====================================================
 HRESULT CTimer::Init(void)
 {
-	m_fSeconds = 0.0f;
+	// 現在時間の初期化
+	m_fSecond = 0.0f;
 
-	// 生成
-	if(m_pNumber == nullptr)
-	m_pNumber = CNumber::Create(TIME_PLACE, TIME_LIMIT);
-
-	if (m_pNumber != nullptr)
-	{
-		// 位置設定
-		m_pNumber->SetPosition(D3DXVECTOR3(TIME_WIDTH, 75.0f, 0.0f));
-
-		// サイズ調整
-		m_pNumber->SetSizeAll(35.0f, 35.0f);
-	}
+	m_fMsecond = 0.0f;
 
 	return S_OK;
 }
@@ -85,10 +109,16 @@ HRESULT CTimer::Init(void)
 //=====================================================
 void CTimer::Uninit(void)
 {
-	if (m_pNumber != nullptr)
+	if (m_pSecond != nullptr)
 	{
-		m_pNumber->Uninit();
-		m_pNumber = nullptr;
+		m_pSecond->Uninit();
+		m_pSecond = nullptr;
+	}
+
+	if (m_pMilliSecond != nullptr)
+	{
+		m_pMilliSecond->Uninit();
+		m_pMilliSecond = nullptr;
 	}
 
 	// 自身の破棄
@@ -103,19 +133,20 @@ void CTimer::Update(void)
 	// デルタタイム取得
 	float fDeltaTime = CManager::GetDeltaTime();
 
-	// タイマー加算
-	m_fSeconds += fDeltaTime;
+	// デルタタイム(リアル時間)加算
+	m_fSecond += fDeltaTime;
 
 	// 秒の計算
-	int nSecond = (int)m_fSeconds % 60;
+	int nSecond = (int)m_fSecond % TIME_CALCULATION;
+
+	// ミリ秒の計算
+	int nMsecond;
 
 	// 秒表示の制御
-	if (m_pNumber != nullptr)
-		m_pNumber->SetValue(nSecond, TIME_PLACE);
+	if (m_pSecond != nullptr)
+		m_pSecond->SetValue(nSecond, SECOND_PLACE);
 
-	// タイマーの上限値超えないように
-	if (nSecond >= TIME_LIMIT)
-		m_fSeconds = TIME_LIMIT;
-
-	//CDebugProc::GetInstance()->Print("\nデルタタイム[%f]", fDeltaTime);
+	// 制限時間の上限値超えないように
+	if (nSecond >= SECOND_LIMIT)
+		m_fSecond = SECOND_LIMIT;
 }
