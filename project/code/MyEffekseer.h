a0a8d20cf8e95a75b2429be05fe543d25a86b340
TakayamaMomoya <119379245+TakayamaMomoya@users.noreply.github.com>
2024-07-12 11:15:28 +0900

    エフェクシアのエフェクトをクラスにしました

--- project/code/MyEffekseer.h
+++ project/code/MyEffekseer.h
@@ -7,10 +7,12 @@
 #ifndef _EFFEKSEER_H_     //このマクロが定義されてなかったら
 #define _EFFEKSEER_H_     //2重インクルードを防止のマクロを定義する
 
+class CEffekseerEffect;
+
 #define MAX_EFK   (64)
 
 class CEffekseer
-{
+{// エフェクシアのマネージャー
 public:
 	CEffekseer();
 	~CEffekseer();
@@ -28,43 +30,64 @@ public:
 		TYPE_MAX
 	};
 
-	// 情報
-	struct Info
-	{
-		Effekseer::Vector3D pos;       // 位置
-		Effekseer::Vector3D rot;       // 向き
-		Effekseer::Vector3D scale;     // 大きさ
-		Effekseer::EffectRef effect;   // エフェクト
-		Effekseer::Handle efkHandle;   // ハンドル
-		const char* EfkName;           // 名前
-		int32_t time;                  // 時間
-	};
-
+	// メンバ関数
 	void Init(void);
 	void Uninit(void);
 	void Update(void);
 	void Draw(void);
 
 	void Set(const char* FileName, ::Effekseer::Vector3D pos, ::Effekseer::Vector3D rot, ::Effekseer::Vector3D scale);
+	CEffekseerEffect *CreateEffect(const char* FileName, ::Effekseer::Vector3D pos, ::Effekseer::Vector3D rot, ::Effekseer::Vector3D scale);
 
 	::EffekseerRendererDX9::RendererRef GetEffekseerRenderer(void) { return m_efkRenderer; }
 	::Effekseer::ManagerRef GetEfkManager(void) { return m_efkManager; }
 	void SetupEffekseerModules(::Effekseer::ManagerRef efkManager);
 
 	void Release(int idx);
+	void ReleaseEffect(CEffekseerEffect *pEffect);	// エフェクトの解放
 
 	static const char* m_apEfkName[CEffekseer::TYPE_MAX];
 
 private:
 
 	// メンバ変数 
-	Info m_Info[MAX_EFK];
 	EffekseerRendererDX9::RendererRef m_efkRenderer;
 	::Effekseer::Matrix44 cameraMatrix;
 	::Effekseer::Matrix44 projectionMatrix;
 	::Effekseer::ManagerRef m_efkManager;
 	::Effekseer::Vector3D viewerPosition;
 	int m_nNum;
+	std::list<CEffekseerEffect*> m_listEffect;	// エフェクトクラスのリスト
+};
+
+class CEffekseerEffect
+{
+public:
+	CEffekseerEffect();	// コンストラクタ
+	~CEffekseerEffect();	// デストラクタ
+
+	// メンバ関数
+	void Init(::Effekseer::Vector3D pos, ::Effekseer::Vector3D rot, ::Effekseer::Vector3D scale);
+	void Uninit(void);
+
+	// 変数取得・設定関数
+	void SetEffect(Effekseer::EffectRef effect) { m_effect = effect; }	// エフェクト参照
+	Effekseer::EffectRef GetEffect(void) { return m_effect; }
+	void SetHandle(Effekseer::Handle handle) { m_efkHandle = handle; }	// ハンドル
+	Effekseer::Handle GetHandle(void) { return m_efkHandle; }
+	void SetTime(int32_t time) { m_time = time; }	// 時間
+	int32_t GetTime(void) { return m_time; }
+	Effekseer::Vector3D GetPosition(void) { return m_pos; }	// 位置の取得
+
+private:
+	// メンバ変数
+	Effekseer::Vector3D m_pos;       // 位置
+	Effekseer::Vector3D m_rot;       // 向き
+	Effekseer::Vector3D m_scale;     // 大きさ
+	Effekseer::EffectRef m_effect;   // エフェクト
+	Effekseer::Handle m_efkHandle;   // ハンドル
+	const char* m_EfkName;           // 名前
+	int32_t m_time;                  // 時間
 };
 
 #endif
